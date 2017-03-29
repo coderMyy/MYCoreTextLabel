4bc4e682edd74f53d8e37f26588b9ee58bd5701a
mengyaoyao <meng.yaoyao@jyall.com>
2017-03-29 14:28:28 +0800

    BUG

--- MYCoreText_Example/MYCoreTextLabel/MYCoreTextLabel.h
+++ MYCoreText_Example/MYCoreTextLabel/MYCoreTextLabel.h
@@ -4,7 +4,13 @@
 //
 //  Created by 孟遥 on 2017/2/5.
 //  Copyright © 2017年 孟遥. All rights reserved.
-//
+
+/**
+ 
+ Github地址 : https://github.com/coderMyy/MYCoreTextLabel 求Star , Fork .....
+ 博客地址    : http://blog.csdn.net/codermy  , 偶尔会记录一下学习的东西 .
+ 
+*/
 
 #import <UIKit/UIKit.h>
 #import "MYCoretextResultTool.h"
--- MYCoreText_Example/MYCoreTextLabel/MYCoretextResultTool.h
+++ MYCoreText_Example/MYCoreTextLabel/MYCoretextResultTool.h
@@ -4,7 +4,14 @@
 //
 //  Created by 孟遥 on 2017/2/11.
 //  Copyright © 2017年 孟遥. All rights reserved.
-//
+
+/**
+ 
+ Github地址 : https://github.com/coderMyy/MYCoreTextLabel 求Star , Fork .....
+ 博客地址    : http://blog.csdn.net/codermy  , 偶尔会记录一下学习的东西 .
+ 
+ */
+
 
 #import <Foundation/Foundation.h>
 #import "MYSubCoretextResult.h"
@@ -24,10 +31,11 @@
 #pragma mark - 设置自定义链接
 + (void)customLinks:(NSArray<NSString *> *)customLinks;
 
+
 /**
  赋值keyword
 
- @param keyword <#keyWord description#>
+ @param keywords <#keywords description#>
  */
 + (void)keyWord:(NSArray<NSString *> *)keywords;
 
